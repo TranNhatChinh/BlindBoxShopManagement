793596a34421bb72f7e6aef9c089186c3b3c21c8
ChinhTN <chinhtrannhatn@gmail.com>
2025-08-01 18:41:20 +0700

    feature(shop management): desgin window

--- BlindBoxShopManagement/ShopManagementWindow.xaml.cs
+++ BlindBoxShopManagement/ShopManagementWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,43 @@ namespace BlindBoxShopManagement
         {
             InitializeComponent();
             this.role = role;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            if (!role.Equals("Admin") && !role.Equals("Manager"))
+            {
+                staffManagementButton.IsEnabled = false;
+                ProductManagementButton.IsEnabled = false;
+            }
+      
+          
+        }
+
+        private void StaffManageClick(object sender, RoutedEventArgs e)
+        {
+            StaffManagementWindow staffManagementWindow = new StaffManagementWindow();
+            staffManagementWindow.Show();
+
+        }
+
+        private void LogOutClick(object sender, RoutedEventArgs e)
+        {
+            Login loginWindow = new Login();
+            loginWindow.Show();
+            this.Close(); // Close the current window
+        }
+
+        private void Button_Order(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProductManagementClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
