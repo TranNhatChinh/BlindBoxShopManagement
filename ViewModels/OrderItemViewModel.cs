37681df7cdd57c3fd91242ca1ab17057bf883f38
ChinhTN <chinhtrannhatn@gmail.com>
2025-08-02 04:42:56 +0700

    feature(Ordering): fix auto update total amount

--- ViewModels/OrderItemViewModel.cs
+++ ViewModels/OrderItemViewModel.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ViewModels
 {
-    public class OrderItemViewModel
+    public class OrderItemViewModel : INotifyPropertyChanged
     {
+        private int quantity;
+
         public string ProductName { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; } = 1;
+
+        public int Quantity
+        {
+            get => quantity;
+            set
+            {
+                if (quantity != value)
+                {
+                    quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                    OnPropertyChanged(nameof(TotalPrice)); // Đảm bảo cột thành tiền cập nhật
+                }
+            }
+        }
 
         public decimal TotalPrice => Price * Quantity;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
 }
